bdc1dee387b423438b4812fdb87faf3b05295055
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-03-18 18:44:36 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,4 @@
+  
 #include "main.h"
 
 /**
@@ -7,10 +8,10 @@
 
 int main(void)
 {
-        int i;
+int i;
 
-        i = 0;
-        printf(positive_or_negative(i));
+i = 0;
+positive_or_negative(i);
 
-        return (0);
+return (0);
 }
