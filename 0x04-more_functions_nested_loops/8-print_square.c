0b17909d068140bb123e5573533fdd3d74b631b7
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-03-18 22:43:44 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * print_square - prints a square of size size
- * @size: size of square
- *
- * Return: void
+ * print_square - a function that prints a square, followed by a new line
+ * @size: size of both width and length
+ * Return: a square made of '#'
  */
-
-
 void print_square(int size)
 {
-	int a, b;
+	int co, ro;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (a = 0; a < size; a++)
 	{
-		for (b = 0; b < (size); b++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
 	}
-
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
