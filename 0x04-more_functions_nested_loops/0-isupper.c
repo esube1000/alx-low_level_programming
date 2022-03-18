ffc7a13b2f016eed64a9388da3ed572f1c45258f
root <gashayeesubalew7@gmail.com>
2022-03-18 01:33:53 -0700

    c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *  * _isupper - define if a character is uppercase
- *   *
- *    * Description: Prints the alphabet with _putchar
- *     * @c: charater
- *      * Return: void
- *       */
+ * _isupper - define if a character is uppercase
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
+ */
 
 int _isupper(int c)
 {
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-my readme
+My 0x04-more_functions_nested_loops
