d7afa0adaed21cbe988d7420f8839aaa844078c8
root <gashayeesubalew7@gmail.com>
2022-03-18 00:47:48 -0700

    s

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 /**
- *  * main - Prints Holberton
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
+ *  *_isupper - checks if letters are uppercase.
+ *   *@c: input.
+ *    *
+ *     *Return: 1 if c is uppercase
+ *      *0 otherwise.
+ *       */
+int _isupper(int c)
 {
-		_putchar('H');
-			_putchar('o');
-				_putchar('l');
-					_putchar('b');
-						_putchar('e');
-							_putchar('r');
-								_putchar('t');
-									_putchar('o');
-										_putchar('n');
-											_putchar('\n');
-												return (0);
+		if ((c >= 'A') && (c <= 'Z'))
+				{
+							return (1);
+								}
+			else
+					{
+								return (0);
+									}
 }
