1bcdc52532df4f50455d2f742ee66ab67c62911c
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-03-24 02:12:02 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,47 +1,24 @@
 #include "main.h"
-
 /**
- *
- * *_strcat - function that concatenates two strings.
- *
- * *@dest: pointer to destination string.
- *
- * *@src: pointer to source string.
- *
- * *
- *
- * *Return: pointer to destination string.
- *
- *  */
-
+*_strcat - function that concatenates two strings.
+*@dest: pointer to destination string.
+*@src: pointer to source string.
+*
+*Return: pointer to destination string.
+ */
 char *_strcat(char *dest, char *src)
-
 {
+int length, j;
 
-	int length, j;
-
-
-
-	length = 0;
-
-	while (dest[length] != '\0')
-
-	{
-
-		length++;
-
-	}
-
-	for (j = 0; src[j] != '\0'; j++, length++)
-
-	{
-
-		dest[length] = src[j];
-
-	}
-
-	dest[length] = '\0';
-
-	return (dest);
-
+length = 0;
+while (dest[length] != '\0')
+{
+length++;
+}
+for (j = 0; src[j] != '\0'; j++, length++)
+{
+dest[length] = src[j];
+}
+dest[length] = '\0';
+return (dest);
 }
