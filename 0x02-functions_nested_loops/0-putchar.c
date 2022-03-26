52e95f54f354dca2a3260889b3ae1a99b61186d9
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-03-26 12:13:19 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,13 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
- * main - Print "main" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-	char main[] = "main";
-	int i = 0;
-
-	while (main[i] != '\0')
-	{
-		_putchar(main[i]);
-		i++;
-	}
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
