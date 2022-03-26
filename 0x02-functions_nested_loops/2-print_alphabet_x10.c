8c214fc252822aa1ab23c14932a9930913d0a512
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-03-26 12:31:30 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * print_alphabet_x10 - Print alphabet in lowercase 10 times
- * Return: 0
- */
+  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * Return: Nothing.
+  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i;
 	char c;
 
-	while (i < 10)
+	for (i = 0; i < 10; ++i)
 	{
-		c = 'a';
-		while (c <= 'z')
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
-			c++;
 		}
 		_putchar('\n');
-		i++;
 	}
 }
