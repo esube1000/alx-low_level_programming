2305997bdde525bf8ac83722df9cabaef69439d6
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-04-02 17:43:05 +0300

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,23 +1,20 @@
-#include "main`.h"
+#include "main.h"
+
 /**
- *_strcat - concatenates two strings
- *@dest: dest
- *@src: src
- *Return: pointer to dest
- **/
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
+ */
 
 char *_strcat(char *dest, char *src)
 {
-int l = 0;
-int i;
-while (dest[l] != '\0')
-{
-++l;
-}
-for (i = 0; src[i] != '\0'; ++i, ++l)
-{
-dest[l] = src[i];
-}
-dest[l] = '\0';
-return (dest);
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
