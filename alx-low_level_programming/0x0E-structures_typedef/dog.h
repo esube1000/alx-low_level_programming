b7b5d40b616fc08d130a6e1f5300992479e9e459
esube1000 <99707031+esube1000@users.noreply.github.com>
2022-04-11 17:58:50 +0300

    Update dog.h

--- alx-low_level_programming/0x0E-structures_typedef/dog.h
+++ alx-low_level_programming/0x0E-structures_typedef/dog.h
@@ -1,18 +1,30 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * dog_t - Typedef for struct dog
  */
-int main(void)
-{
-    struct dog my_dog;
+typedef struct dog dog_t;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
-}
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
